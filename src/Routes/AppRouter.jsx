429515d6930b7653e0742747b1335e69da520e96
juaniandrada23 <juanandrada_07_@hotmail.com>
2023-12-07 20:58:46 -0300

    Cambio de ventana ppal

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Principal from '../Components/Principal';
 import Proveedores from './Proveedores';
 import Pagos from './Pagos';
 import Calculos from './Calculos';
 import MediosPago from './MediosPago.jsx'
 import PruebaLogin from './PruebaLogin.jsx'
 import Usuarios from './Usuarios';
-import ProbandoPrincipal from '../Custom/ProbandoPrincipal.jsx'
+import ProbandoPrincipal from './ProbandoPrincipal.jsx'
+import Pruebas from '../Custom/Pruebas.jsx';
 
 const AppRouter = () => {
   return (
@@ -15,13 +15,13 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<PruebaLogin/>} />
         <Route path="/usuarios/:userId" element={<Usuarios/>} />
-        <Route path="/principal/:userId" element={<Principal/>} />
         <Route path="/proveedores/:userId" element={<Proveedores/>} />
         <Route path="/calculos/:userId" element={<Calculos/>} />
         <Route path="/pagos/:userId" element={<Pagos/>} />
         <Route path="/medios/:userId" element={<MediosPago/>} />
+        <Route path="/probandoprincipal/:userId" element={<ProbandoPrincipal/>} />
 
-        <Route path="/probandoprincipal" element={<ProbandoPrincipal/>} />
+        <Route path="/pruebas" element={<Pruebas/>} />
       </Routes>
     </Router>
   );
